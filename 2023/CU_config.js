1a89b9286398668f39b7ca0dc32173c03746156a
guusbuk <guus.bukkems@chello.nl>
2023-01-25 12:30:38 +0100

    event=2019

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2022carv",
+      "defaultValue": "2019casf",
       "required": "true",
       "disabled": "true"
     },
