ae152edb1a53392d8633f85dff3dec024a426500
guusbuk <guus.bukkems@chello.nl>
2023-01-25 12:24:15 +0100

    Update TBAInterface.js

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "BVktw3I2fReBVSKC3yGr9lyzofiRlcKs8nbZLj8Kel8VDHPSgzICmkAkogoOGlq3";
 /**
  * Get list of teams in event
  *
